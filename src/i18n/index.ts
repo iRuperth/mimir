ba4e07f10a227303ce0bc02ad7b4bff3c8842bff
iRuperth <ruperthlosada@gmail.com>
2026-05-18 23:14:44 +0200

    feat: wire i18next with English Spanish locales and base stylesheets

---
+++ src/i18n/index.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import { config } from '@/config/env';
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+void i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    fallbackLng: config.language.default,
+    supportedLngs: ['en', 'es'],
+    interpolation: { escapeValue: false },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      lookupLocalStorage: 'mimir-lang',
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
